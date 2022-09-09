35dd5188bbb2eba9915339e416cec38ca606b37d
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-09-09 15:58:49 +0200

    add nutrition sum

---
+++ p-12-sportsee/src/components/userNutritionSum/userNutritionSum.js
@@ -0,0 +1,43 @@
+import React from "react"
+import Calories from '../../assets/calories-icon.svg'
+import Carbs from '../../assets/carbs-icon.svg'
+import Fat from '../../assets/fat-icon.svg'
+import Protein from '../../assets/protein-icon.svg'
+import './userNutritionSum.css'
+
+function UserNutritionSum (props){
+    return(
+        <div className='nutrition-container'>
+            <div className='container'>
+                <img src={Calories} className='container-img' alt="Calories"/>
+                <div className='container-infos'>
+                    <span><strong>{props.usernutritionsum.calorieCount} kCal</strong></span>
+                    <span>Calories</span>
+                </div>
+            </div>
+            <div className='container'>
+                <img src={Protein} className='container-img' alt="Carbs"/>
+                <div className='container-infos'>
+                    <span><strong>{props.usernutritionsum.carbohydrateCount} g</strong></span>
+                    <span>Proteines</span>
+                </div>    
+            </div>
+            <div className='container'>
+                <img src={Carbs} className='container-img' alt="Fat"/>
+                <div className='container-infos'>
+                    <span><strong>{props.usernutritionsum.lipidCount} g</strong></span>
+                    <span>Glucides</span>
+                </div>    
+            </div>
+            <div className='container'>
+                <img src={Fat} className='container-img' alt="Protein"/>
+                <div className='container-infos'>
+                    <span><strong>{props.usernutritionsum.proteinCount} g</strong></span>
+                    <span>Lipides</span>
+                </div>    
+            </div>
+        </div>
+    )
+}
+
+export default UserNutritionSum
