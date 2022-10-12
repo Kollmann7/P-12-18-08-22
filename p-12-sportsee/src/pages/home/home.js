fe3fe501f8736730a101b9e908fbf5e08d88f620
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-10-12 11:04:23 +0200

    display a link to the users

---
+++ p-12-sportsee/src/pages/home/home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from '../../components/header/header'
+import SideBar from '../../components/sidebar/sidebar'
+import { Link } from 'react-router-dom'
+import './home.css'
+
+/**
+  * @return {ReactElement}
+  */
+function Home() {
+  return (
+    <div>
+      <Header />
+          <div className='user-wrapper'>
+            <Link to="/user/12">
+              <div className='user-display'>user 12</div>
+            </Link>
+            <Link to="/user/18">
+              <div className='user-display'>user 18</div>
+            </Link>
+          </div>
+      <SideBar />
+    </div>
+  )
+}
+
+export default Home
